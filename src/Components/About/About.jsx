10acb03bb7e05e046779cb48f8ab038fb9d1830d
HP <samidev336@gmail.com>
2025-07-20 13:02:15 +0500

    About-me section

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg';
+import Profile_img from '../../assets/about_profile.svg';
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-title">
+            <h1>About Me</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+        <div className="about-section">
+            <div className="about-left">
+                <img src={Profile_img} alt="" />
+            </div>
+            <div className="about-right">
+                <div className="about-para">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus necessitatibus architecto cum id laborum ipsa! Sunt dignissimos libero quibusdam iste maxime repudiandae nisi qui quas, architecto vitae dolorem veniam placeat.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A reprehenderit, quaerat nesciunt molestias repellendus at provident nisi corporis ex eveniet ut minima suscipit qui! Neque corporis nemo architecto tempora ad?</p>
+                </div>
+                <div className="about-skills">
+                    <div className="about-skill">
+                        <p>HTML & CSS</p>
+                        <hr style={{width:'50%'}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>React JS</p>
+                        <hr style={{width:'70%'}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>JavaScript</p>
+                        <hr style={{width:'60%'}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>Next JS</p>
+                        <hr style={{width:'50%'}} />
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <div className="about-achievments">
+            <div className="about-achievment">
+                <h1>10+</h1>
+                <p>YEARS OF EXPERIENCE</p>
+            </div>
+            <hr />
+            <div className="about-achievment">
+                <h1>90+</h1>
+                <p>PROJECTS COMPLETED </p>
+            </div>
+            <hr />
+            <div className="about-achievment">
+                <h1>15+</h1>
+                <p>Happy Clients</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
