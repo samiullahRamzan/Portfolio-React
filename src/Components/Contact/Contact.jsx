030630108446a3bb1066f73e476cab0ce10294fe
HP <samidev336@gmail.com>
2025-07-20 16:24:29 +0500

    contact

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import theme_pattern from '../../assets/theme_pattern.svg';
+import mail_icon from '../../assets/mail_icon.svg';
+import location_icon from '../../assets/location_icon.svg';
+import call_icon from '../../assets/call_icon.svg';
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+        <div className="contact-title">
+            <h1>Get in touch</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+        <div className="contact-section">
+            <div className="contact-left">
+                <h1>Let's Talk</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi quas fugiat veritatis ad maxime magnam repudiandae quaerat aliquam saepe, placeat, est suscipit laborum ratione id aliquid maiores culpa illum. A.</p>
+                <div className="contact-details">
+                    <div className="contact-detail"><img src={mail_icon} alt="" /><p>samidev336@gmail.com</p></div>
+                    <div className="contact-detail"><img src={call_icon} alt="" /><p>+92307002179</p></div>
+                    <div className="contact-detail"><img src={location_icon} alt="" /><p>CA, United States</p></div>
+                </div>
+            </div>
+            <form className="contact-right">
+                <label htmlFor="">Your Name</label>
+                <input type="text" placeholder='Enter your name' name='name' />
+                <label htmlFor="">Your Email</label>
+                <input type="email" placeholder='Enter your email' name='email' />
+                <label htmlFor="">Write your message here</label>
+                <textarea name="message" rows='8' placeholder='Enter your message'></textarea>
+                <button className="contact-submit" type='submit'>Submit now</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
